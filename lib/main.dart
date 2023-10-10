2a07de4e8035faf9d8f853f7cfbf97011e3ecc95
nobel-0022 <83616937+nobel-0022@users.noreply.github.com>
2023-10-10 12:52:50 -0700

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,110 +1,218 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
+/// Navigation - one page -> another page
+/// Navigator 1(Simple and Lengthy), 2 (complex)
+/// Routers Package -> Getx, GoRouter, Auto Route
 
+/// TODO : Stack, Queue, Navigator, Navigation
+
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'appTitle',
-      home: MyHomePage(title: "appTitle"),
+      home: HomeScreen(),
     );
   }
-
 }
-class MyHomePage extends StatelessWidget{
-  final String  title;
 
- const MyHomePage({Key? key , required this.title}): super(key: key);
-    @override
-  Widget build(BuildContext context){
-      return Scaffold(
+/// Route
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-      title: Text("title"),
-    backgroundColor: Colors.green,
+        title: Text('Home'),
       ),
-        body: const Center(
-          child: Text(
-            "A drawer is an invisible side screen",
-            style: TextStyle(fontSize: 20.0),
-          ),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.all(0),
-            children: [
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.green
-                ),
-                child: UserAccountsDrawerHeader(
-                  decoration: BoxDecoration(color: Colors.green),
-                  accountName: Text(
-                    "Nobel dey babu",
-                    style: TextStyle(fontSize: 18),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Home',
+              style: TextStyle(fontSize: 24),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                /// Navigation - Route home -> Route settings
+                /// Navigator
+                /// Navigator.typeOfNavigation(currentLocation, Destination);
+                /// Navigation - Push, Pop(Back), replace, replaceAll, removeUntil
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SettingsScreen(),
                   ),
-                  accountEmail: Text("nobeldeybabu@gmail.com"),
-                  currentAccountPictureSize: Size.square(50),
-                  currentAccountPicture: CircleAvatar(
-                    backgroundColor: Color.fromARGB(255, 165, 255, 137),
-                    child: Text(
-                      "A",
-                      style: TextStyle(fontSize: 30.0, color: Colors.blue),
+                );
+              },
+              child: Text('Go to settings'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                /// Navigation - Route home -> Route settings
+                /// Navigator
+                /// Navigator.typeOfNavigation(currentLocation, Destination);
+                /// Navigation - Push, Pop(Back), replace, replaceAll, removeUntil
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => OrdersScreen(),
+                  ),
+                );
+              },
+              child: Text('Go to Orders'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                /// Navigation - Route home -> Route settings
+                /// Navigator
+                /// Navigator.typeOfNavigation(currentLocation, Destination);
+                /// Navigation - Push, Pop(Back), replace, replaceAll, removeUntil
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>ProductListScreen()),);
+              },
+              child: Text('Go to Product list'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-                    ),
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.person),
-                title: Text("My Profile"),
-                onTap: (){
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.book),
-                title: Text('My Course'),
-                onTap: (){
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.workspace_premium),
-                title: Text("Go premium"),
-                onTap: (){
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.video_label),
-                title: Text("Saved Videos"),
-                onTap: (){
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.edit),
-                title: Text("Edit Profile"),
-                onTap: (){
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.logout),
-                title: Text("Logout"),
-                onTap: (){
-                  Navigator.pop(context);
-                },
-              )
-            ],
-          ),
+/// Route
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Settings',
+              style: TextStyle(fontSize: 24),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(context,
+                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                      (route) => false,
+                );
+              },
+              child: Text('Home'),
+            )
+          ],
         ),
-      );
+      ),
+    );
   }
+}
+
+/// Route
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Orders'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Orders',
+              style: TextStyle(fontSize: 24),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SettingsScreen(),
+                  ),
+                );
+              },
+              child: Text('Go to settings'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Back to home'),
+            )
+          ],
+        ),
+      ),
+    );
   }
+}
+
+class ProductListScreen extends StatelessWidget{
+  const ProductListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product list'),
+      ),
+      body: ListView.builder(
+        itemCount: 20,
+          itemBuilder: (context, index){
+        return ListTile(
+          onTap: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return ProductDetailsScreen(productName: index.toString(),price: 233,);
+            }),).then((value){
+              print(value);
+            });
+          },
+          title: Text(index.toString()),
+          subtitle: Text('Product details $index'),
+        );
+      }),
+    );
+  }
+}
+class ProductDetailsScreen extends StatelessWidget{
+  final String productName;
+  final double? price;
+  const ProductDetailsScreen({super.key, required this.productName, this.price});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Details'),
+      ),
+       body: Center(
+         child: Column(
+           children: [
+             Text(productName, style: TextStyle(
+               fontSize: 24,
+             ),),
+             ElevatedButton(onPressed: (){
+               Navigator.pop(context,'my-name');
+             }, child: Text('Back'),),
+           ],
+         ),
+
+       ),
+    );
+  }
+}
